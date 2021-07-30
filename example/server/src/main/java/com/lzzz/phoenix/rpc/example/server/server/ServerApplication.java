8fd1741054515e697b73ef1d37c3cc2c1e174da7
luzhong <luzhong256@gmail.com>
2021-07-30 15:20:12 +0800

    feat(rpc): 完善rpc调用逻辑

---
+++ example/server/src/main/java/com/lzzz/phoenix/rpc/example/server/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.lzzz.phoenix.rpc.example.server.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
